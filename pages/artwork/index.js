22374f65251c43a458d6b86e7b29a3b26185bfae
Joshua <joshuawjh@hotmail.com>
2023-08-04 01:53:50 -0400

    a6-3

--- pages/artwork/index.js
+++ pages/artwork/index.js
@@ -1,76 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import {useState, useEffect} from 'react';
+import {useRouter} from 'next/router';
 import useSWR from 'swr';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Error from 'next/error';
-import Pagination from 'react-bootstrap/Pagination';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
-import ArtworkCard from '@/components/ArtworkCard';
-import validObjectIDList from '@/public/data/validObjectIDList.json';
+import Pagination from 'react-bootstrap/Pagination';
+import ArtworkCard from '../components/ArtworkCard';
+import validObjectIDList from '../../public/data/validObjectIDList.json';
 
 const PER_PAGE = 12;
 
-export default function ArtHome() {
+export default function ArtworkIndex(){
+    const [artworkList, setArtworkList] = useState();
+    const [page, setPage] = useState(1);
     const router = useRouter();
     let finalQuery = router.asPath.split('?')[1];
-    let [ artworkList, setArtworkList ] = useState();
-    let [ page, setPage ] = useState(1)
-    const { data, error } = useSWR(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${finalQuery}`);
-      
+    const {data, error} = useSWR(`https://collectionapi.metmuseum.org/public/collection/v1/search?${finalQuery}`);
+
+    function previousPage(){
+        if (page > 1) {
+            setPage(page - 1);
+        }
+    }
+
+    function nextPage(){
+        if (page < artworkList.length) {
+            setPage(page + 1);
+        }
+    }
+
     useEffect(() => {
-        if (data != null && data != undefined) {
+        if (data) {
             let filteredResults = validObjectIDList.objectIDs.filter(x => data.objectIDs?.includes(x));
-            let results = []
-            for (let i = 0; i < filteredResults.length; i += PER_PAGE) {
-                const chunk = filteredResults.slice(i, i + PER_PAGE);
+            let results = [];
+            for (let i = 0; i < filteredResults?.length; i += PER_PAGE){
+                const chunk = filteredResults?.slice(i, i + PER_PAGE);
                 results.push(chunk);
-            }            
+            }
+            console.log(filteredResults);
             setArtworkList(results);
             setPage(1);
         }
     }, [data]);
 
     if (error) {
-        return <Error statusCode={404} />;
-    } 
+        return (
+            <Error statusCode={404}/>
+        );
+    }
 
-    const nextPage = () => page < artworkList.length && setPage(++page);
-    const previousPage = () => page > 1 && setPage(--page);
+    if (artworkList && artworkList.length > 0){   
+        let artworks = [];
+        artworkList[page - 1].forEach((artwork) => {
+            artworks.push(
+                <Col lg={3} eventKey={artwork} key={artwork}>
+                    <ArtworkCard objectID={Number(artwork)}/>
+                </Col>
+            );
+        });
 
-    return (
-        <>
-            <Container>
-            {artworkList.length > 0 ? (
-                <>
-                    <Row className="gy-4">
-                        {artworkList[page - 1].map((currentObjectID) => (
-                            <Col lg={3} key={currentObjectID}>
-                    <ArtworkCard objectID={currentObjectID} />
-                            </Col>
-                        ))}
-                    </Row>
-                    <Row>
-                        <Col>
-                            <br /><br />
-                            <Pagination>
-                                <Pagination.Prev onClick={previousPage} />
-                                <Pagination.Item>{page}</Pagination.Item>
-                                <Pagination.Next onClick={nextPage} />
-                            </Pagination>
-                        </Col>
-                    </Row>
-                </>
-                ) : (
-                <Card>
-                    <Card.Body>
-                        <h4>Nothing Here</h4>
-                        <p>Try searching for something else.</p>
-                    </Card.Body>
-                </Card>
-                )}
-            </Container>
-        </>
-    );
+        console.log(artworks);
+
+        return (
+            <>
+                <Row className="gy-4">
+                    {artworks}
+                </Row>
+                <Row>
+                    <Col>
+                        <Pagination>
+                            <Pagination.Prev onClick={previousPage}/>
+                            <Pagination.Item>{page}</Pagination.Item>
+                            <Pagination.Next onClick={nextPage}/>
+                        </Pagination>
+                    </Col>
+                </Row>
+            </>
+        );
+    }  else if (!artworkList) {
+        return null;
+    }   else {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Text>
+                        <h4>Nothing Here</h4> Try searching for something else.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
 }
